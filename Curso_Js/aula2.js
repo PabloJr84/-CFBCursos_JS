8143b9b074868fac8582c12eb1e0cc2cf6680def
Pablo Assis <52177225+PabloJr84@users.noreply.github.com>
2023-08-21 13:40:02 -0300

    Javascript em Modo Estrito, você sabe o que é? - Curso de Javascript Moderno - Aula 02

---
+++ Curso_Js/aula2.js
@@ -0,0 +1,16 @@
+//Javascript em Modo Estrito, você sabe o que é?
+/*Javascript em Modo Estrito, você sabe o que é? - Curso de Javascript Moderno - Aula 02
+
+Hoje em nosso curso de Javascript Completo e Profissional vamos aprender o que é o modo estrito, 
+usando o comando "use strict". O modo estrito ou strict mode faz com que a programação seja mais limpa e funcional.
+
+JavaScript é uma linguagem de programação interpretada estruturada, 
+de script em alto nível com tipagem dinâmica fraca e multiparadigma. 
+Juntamente com HTML e CSS, o JavaScript é uma das três principais tecnologias para desenvolvimento de aplicações Web e Sites.
+*/
+"use strict" 
+ 
+let nome="Pablo";
+console.log("CFB Curso")
+console.log(nome);
+console.log("Nome :" + nome);
